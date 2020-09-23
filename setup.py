d44537b74e9f426a00eb94ed0f93790fad1d89b3
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-09-23 21:59:05 +0200

    release 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="checksec.py",
-    version="0.0.1",
+    version="0.0.2",
     author="Mathieu Tarral",
     author_email="mathieu.tarral@protonmail.com",
     description="Checksec tool implemented in Python",
