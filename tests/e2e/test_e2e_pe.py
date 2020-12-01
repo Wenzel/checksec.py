e1e0aeb4439c0a338a4897b0a1f34fa73326ad29
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-12-01 12:04:41 +0100

    tests: add e2e tests for PE

---
+++ tests/e2e/test_e2e_pe.py
@@ -0,0 +1,31 @@
+"""E2E tests for PE"""
+
+from pathlib import Path
+
+import pytest
+
+from tests.conftest import run_checksec
+
+PE_BINARIES = Path(__file__).parent.parent / "binaries" / "pe"
+
+
+@pytest.mark.parametrize("is_enabled", [False, True])
+@pytest.mark.parametrize(
+    "prop",
+    [
+        "nx",
+        "canary",
+        "dynamic_base",
+        "aslr",
+        "high_entropy_va",
+        "safe_seh",
+        "force_integrity",
+        "guard_cf",
+        "isolation",
+    ],
+)
+def test_bool_prop(prop: str, is_enabled: bool):
+    """Test that boolean prop is disabled/enabled"""
+    bin_path = PE_BINARIES / f"{prop}_{'enabled' if is_enabled else 'disabled'}.exe"
+    chk_data = run_checksec(bin_path)
+    assert chk_data[str(bin_path)][prop] == is_enabled
